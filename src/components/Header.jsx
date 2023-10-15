f7c5265fe9bfe106de2b2c23cc70eba981ebc365
rahulpandit123 <rahulpanditercs01@gmail.com>
2023-10-15 17:50:15 +0530

    added resume download button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import resume from '../assets/CV_Rahul2023.pdf';
 import { AiOutlineMenu } from 'react-icons/ai';
 const Header = ({menuOpen, setMenuOpen}) => {
   return (
@@ -35,10 +36,14 @@ export const NavContent = ({menuOpen,setMenuOpen}) => (
         <a onClick={() => setMenuOpen(false)} href='#services'>Services</a>
         <a onClick={() => setMenuOpen(false)} href='#testimonial'>Testimonial</a>
         <a onClick={() => setMenuOpen(false)}  href='#contact'>Contact</a>
+        
     </div>
     <a href='mailto:rahulpanditercs01@gmail.com'>
         <button>Email</button>
     </a>
+    <button className='btn1'>
+        <a className='btn2' href={resume} download='CV_Rahul2023.pdf'>Download Resume</a>
+    </button>
     </>
 );
 
